c3e698bc3da07a6ee9fccbcf4f887273e20c4d5d
boevoivertolet <bjha78rus@yandex.ru>
2022-09-29 17:47:44 +0300

    Create rootReducer/store from redux
    type RootReducerType = ReturnType<typeof rootReducer>

--- social-network/src/components/ReduxStore/ReduxStore.ts
+++ social-network/src/components/ReduxStore/ReduxStore.ts
@@ -1,8 +1,26 @@
+import {combineReducers, legacy_createStore} from 'redux';
+import profilePageReducer from '../Reducers/profilePageReducer';
+import dialogsPageReducer from '../Reducers/dialogsPageReducer';
+import sidebarReducer from '../Reducers/sidebarReducer';
 
 
 
+let rootReducer = combineReducers(
+    {
+        profilePage: profilePageReducer,
+        dialogsPage: dialogsPageReducer,
+        sidebar: sidebarReducer
+    }
+)
 
+export type RootReducerType = ReturnType<typeof rootReducer>
+
+
+let store: RootReducerType = legacy_createStore(rootReducer);
 
 
 // @ts-ignore
 window.store = store
+
+
+export default store;
