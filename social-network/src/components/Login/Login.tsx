90113031e105d161d3d4478e0ada328613919b1b
boevoivertolet <bjha78rus@yandex.ru>
2023-02-02 11:11:29 +0300

    InjectedFormProps<LoginFormDataType>

--- social-network/src/components/Login/Login.tsx
+++ social-network/src/components/Login/Login.tsx
@@ -1,31 +1,46 @@
-import React from 'react';
-import {LoginType} from '../Types/Types';
-import styles from './Login.module.css'
-import {reduxForm} from 'redux-form'
+import React from "react";
+import { LoginType } from "../Types/Types";
+import styles from "./Login.module.css";
+import { reduxForm, Field, InjectedFormProps } from "redux-form";
 
-
-const LoginForm = () => {
-    return <form action="">
-        <div>
-            <input type="text" placeholder={'login'}/>
-        </div>
-        <div>
-            <input type="text" placeholder={'password'}/>
-        </div>
-        <div>
-            <input type="checkbox"/> remember me
-        </div>
-        <div>
-            <button>login</button>
-        </div>
+const LoginForm: React.FC<InjectedFormProps<LoginFormDataType>> = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field placeholder={"login"} name={"login"} component={"input"} />
+      </div>
+      <div>
+        <Field placeholder={"password"} name={"password"} component={"input"} />
+      </div>
+      <div>
+        <Field component={"input"} type="checkbox" name={"rememberMe"} />{" "}
+        remember me
+      </div>
+      <div>
+        <button>login</button>
+      </div>
     </form>
-}
-const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+  );
+};
+const LoginReduxForm = reduxForm<LoginFormDataType>({ form: "login" })(
+  LoginForm
+);
+console.log(LoginReduxForm);
 
-export const Login = (props: LoginType) => {
-    return <div className={styles.login}>
-        <h1>Login</h1>
-        <LoginReduxForm/>
+export const Login = () => {
+  const onSubmit = (formData: LoginFormDataType) => {
+    console.log(formData);
+  };
+  return (
+    <div className={styles.login}>
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
     </div>
+  );
+};
 
-}
+type LoginFormDataType = {
+  login: string;
+  password: string;
+  rememberMe: boolean;
+};
